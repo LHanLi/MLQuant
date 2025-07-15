34bbd8c96e58efedca274e0028366d1b6bb65602
Linhan Li <h1nlee@163.com>
2025-07-15 16:05:41 +0800

    Update modeling.py
    model可以接收当前窗口开始结束时间

--- MLQuant/modeling.py
+++ MLQuant/modeling.py
@@ -10,10 +10,7 @@ class Modeling():
     def __init__(self, Param, Data=None, \
                  model=None, featureFilter=None, logLoc=None):
         self.Param = Param  # {'trainParam':***, 'featureParam':***, 'modelParam':***, 'logLoc':***}
-        if type(Data)!=type(None):
-            self.Data = Data.reset_index(drop=True)  # 避免index不唯一
-        else:
-            Param['trainParam']
+        self.Data = Data
         if type(featureFilter)!=type(None):
             self.featureFilter = featureFilter # class featureFilter 
         else:
@@ -21,11 +18,13 @@ class Modeling():
         if type(model)!=type(None):
             self.model = model # class Model
         else:
-            self.model = MLQ.io.importMyClass(Param['featureParam']['selectModel'])
+            self.model = MLQ.io.importMyClass(Param['modelParam']['selectModel'])
         if 'logLoc' not in Param.keys():
             self.logLoc = logLoc
     def run(self): 
-        MLQ.io.log("开始Modeling") 
+        MLQ.io.log("加载数据") 
+        self.loadData()
+        MLQ.io.log("开始滑动窗口建模") 
         # 1. 生成滑动训练/测试窗口;
         self.rollingWindow = self.getRollingWindow(\
             self.Param['trainParam']['startResultDate'], self.Param['trainParam']['endResultDate'], \
@@ -33,17 +32,28 @@ class Modeling():
         # 2. 滚动窗口样本内外训练测试;
         for trainstart, trainend, testsart, testend in self.rollingwindow:
             MLQ.io.log(f"train start at {trainstart} end at {trainend}, test start at {testsart} end at {testend}", logLoc=self.logLoc)
-            filter = self.featureFilter(self.Param['featureParam'], trainstart, trainend)  #   a. 创建featureSelection对象
+            filter = self.featureFilter(self.Param['featureParam'], \
+                            trainstart, trainend)  #   a. 创建featureSelection对象
             featureNames = filter.filtFeature()
             self.saveFilter(filter)  # 保存因子过滤器self.store
+            self.Param['modelParam']['_trainstart'] = trainstart
+            self.Param['modelParam']['_trainend'] = trainend # 在Param['modelParam']中加入该窗口开始结束日期（为了兼容generalModeling）
             model = self.model(featureNames, self.Param['modelParam']) #   b. 创建Model对象, 
-            Xi = self.Data[featureNames]
-            Yi = self.Data  # 获取该模型的训练数据 
+            Datai = self.Data[(self.Data['date']<=trainend)&\
+                              (self.Data['date']>=trainstart)] # 该段训练数据
+            Xi = Datai[featureNames]
+            Yi = Datai[self.Param['predict_label']]  # 获取该模型的训练数据 
             model.train(Xi, Yi)      # 训练模型
             self.saveModel(model) #   c. 保存self.model及self.store 
             self.evaluateModel(model) #   c. 模型样本内评价 model 
             self.testModel(model) #   d. 样本外模型预测 result
         self.getReport()# 3. 生成模型报告
+    # 加载数据
+    def loadData(self):
+        if type(self.Data)==type(pd.Dataframe):
+            pass
+        else:
+            self.Data = pd.read_parquet(self.Param['featureDir']) # 从文件中读取数据
     # 生成滑动训练窗口
     def getRollingWindow(self, startResultDate, endResultDate, \
                       trainLen=750, testLen=50, step=5, tradeDates=None):
